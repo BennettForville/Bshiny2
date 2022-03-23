17d355dad2c3acf6efc928cacc189116ec23683e
BennettForville <92814830+BennettForville@users.noreply.github.com>
2022-03-23 16:44:57 -0400

    working on shiny

--- app.R
+++ app.R
@@ -1,6 +1,9 @@
 library(shiny)
 library(hector)
 library(ggplot2)
+library(dplyr)
+
+
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
@@ -33,26 +36,39 @@ ui <- fluidPage(
   )
 )
 
+# SSP45 <- system.file("input/hector_ssp245.ini", package = "hector")
+# reference_plot <- newcore(SSP45)
+# run(reference_plot)
+# #setvar(base_core45, NA, Q10_RH(), q10, getunits(Q10_RH()))
+# #fortify(reference_plot)
+# shutdown(reference_plot)
+
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
-  
-
-  output$distPlot <- renderPlot({
+    output$distPlot <- renderPlot({
     
     q10 <- input$Q10
     
     RCP45 <- system.file("input/hector_ssp245.ini", package = "hector")
     core45 <- newcore(RCP45)
+    reference <- fetchvars(core45, NA)
+    reference$source <- "reference"
     setvar(core45, NA, Q10_RH(), q10, getunits(Q10_RH()))
     run(core45)
     result <- fetchvars(core45, 2000:2200)
+    result$source <- "user_input"
     shutdown(core45)
     
-    ggplot(result, aes(year, value))+
+    output <- bind_rows(reference,result)
+    
+    ggplot(output, aes(year, value, color = source))+
       geom_point()+
       facet_wrap(~variable, scales = "free") + 
       ggtitle(q10)
+    #+
+     # geom_line(data = reference)
+      
   })
   
 }
