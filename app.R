464c5e0fd90cbb7b0a33782f88aba4afe3ac3b80
BennettForville <92814830+BennettForville@users.noreply.github.com>
2022-04-22 13:58:01 -0400

    updated app.r
    Added tabs, put graphs specifically under graph tab, hover bar info, graph labels

--- app.R
+++ app.R
@@ -20,7 +20,7 @@ SSP_files <- c("input/hector_ssp119.ini",
 names(SSP_files) <- scenario_choices
 
 
-variables <- c(ATMOSPHERIC_CO2(), GLOBAL_TEMP(), SOIL_C(), PH_HL(), PH_LL())
+variables <- c(ATMOSPHERIC_CO2(), PH_HL(), SOIL_C(), GLOBAL_TEMP())
 
 # https://stackoverflow.com/questions/36132204/reactive-radiobuttons-with-tooltipbs-in-shiny
 # This function allows for multiple unique hover bars within a radioButtons call
@@ -54,107 +54,116 @@ ui <- fluidPage(
   # App title ----
   titlePanel("What does our future look like?"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+  # Main panel for displaying outputs ----
+  mainPanel(
     
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+    tabsetPanel(type = "tabs",
+                tabPanel("Home", verbatimTextOutput("Home"),
+                         mainPanel("This will be baisc climate info & explanation of important terms")),
+                
+                
+                tabPanel("graphs", 
+                         # Sidebar layout with input and output definitions ----
+                         sidebarLayout(
+                          
+                  
+                           
+                           
+                           
+                           # Sidebar panel for inputs ----
+                           sidebarPanel(
+                             
+                             # Input: Slider for the value of q10 ----
+                             sliderInput(inputId = "Q10",
+                                         label = "Choose a Q10 value:",
+                                         min = 1,
+                                         max = 5,
+                                         value = 2),
+                             
+                             fluidRow(
+                               column(12,
+                                      radioButtons(inputId = "SSP", 
+                                                   label = "Select a specific scenario (SSP):", 
+                                                   choices = scenario_choices,
+                                                   selected = "Scenario 2 - Middle of the road")
+                                      
+                               ),
+                               # id: same inputId as above (very important for reactivity)
+                               # title: text that will appear in the hover bar
+                               # choice: from the list of scenario_choices passed into radioButtons
+                               # placement: optional, but where the hover bar will appear relative to text
+                               radioTooltip(id = "SSP", 
+                                            title = "SSP119 - lots of change to daily life, slow improvement",
+                                            choice = "Scenario 1 - Green hippie world",
+                                            placement = "right"),
+                               radioTooltip(id = "SSP",
+                                            title = "SSP245 - Not much change to daily life, no improvement",
+                                            choice = "Scenario 2 - Middle of the road",
+                                            placement = "right"),
+                               radioTooltip(id = "SSP",
+                                            title = "SSP370 - Focus on food & energy security, environmental degradation",
+                                            choice = "Scenario 3 - A rocky road",
+                                            placement = "right"),
+                               radioTooltip(id = "SSP",
+                                            title = "SSP460 - Unequal investments, rich get richer, poor get poorer",
+                                            choice = "Scenario 4 - Inequality",
+                                            placement = "right"),
+                               radioTooltip(id = "SSP",
+                                            title = "SSP585 - Human & global capital become main focus, fossil fuels are managed",
+                                            choice = "Scenario 5 -  Armageddon",
+                                            placement = "right")
+                             )),
+                           mainPanel(plotOutput(outputId = "distPlot")))),
+                tabPanel("impacts", verbatimTextOutput("impacts"),
+                         mainPanel("This will be overall impacts on climate and big take aways from app"))
+                )))
+                             
+                           
+    
+    
+    
+    
+    # Define server logic required to draw a histogram ----
+    server <- function(input, output) {
       
-      # Input: Slider for the value of q10 ----
-      sliderInput(inputId = "Q10",
-                  label = "Choose a Q10 value:",
-                  min = 1,
-                  max = 5,
-                  value = 2),
-      
-      fluidRow(
-        column(12,
-               radioButtons(inputId = "SSP", 
-                            label = "Select a specific scenario (SSP):", 
-                            choices = scenario_choices,
-                            selected = "Scenario 2 - Middle of the road")
-
-        ),
-        # id: same inputId as above (very important for reactivity)
-        # title: text that will appear in the hover bar
-        # choice: from the list of scenario_choices passed into radioButtons
-        # placement: optional, but where the hover bar will appear relative to text
-        radioTooltip(id = "SSP", 
-                     title = "SSP119",
-                     choice = "Scenario 1 - Green hippie world",
-                     placement = "right"),
-        radioTooltip(id = "SSP",
-                     title = "SSP245",
-                     choice = "Scenario 2 - Middle of the road",
-                     placement = "right"),
-        radioTooltip(id = "SSP",
-                     title = "SSP370",
-                     choice = "Scenario 3 - A rocky road",
-                     placement = "right"),
-        radioTooltip(id = "SSP",
-                     title = "SSP460",
-                     choice = "Scenario 4 - Inequality",
-                     placement = "right"),
-        radioTooltip(id = "SSP",
-                     title = "SSP585",
-                     choice = "Scenario 5 -  Armageddon",
-                     placement = "right")
-      ),
-      
-    ),
+      output$distPlot <- renderPlot({
+        
+        q10 <- input$Q10
+        
+        file <- SSP_files[input$SSP]
+        Sc <- system.file(file, package = "hector")
+        core <- newcore(Sc)
+        run(core)
+        reference <- fetchvars(core, 2000:2200, variables)
+        reference$source <- "reference"
+        setvar(core, NA, Q10_RH(), q10, getunits(Q10_RH()))
+        reset(core)
+        run(core)
+        result <- fetchvars(core, 2000:2200, variables)
+        result$source <- "user_input"
+        shutdown(core)
+        
+        output <- bind_rows(reference, result)
+        
+        var_labels <- c("Atmospheric C", "Ocean pH", "Soil C", "Temperature increase")
+        names(var_labels) <- variables
+        
+        ggplot(output, aes(year, value, color = source, linetype = units)) +
+          geom_line() +
+          facet_wrap(~variable, 
+                     scales = "free",
+                     labeller = labeller(variable = var_labels)) + 
+          ggtitle(paste0("Q10 = ", q10, " in scenario ", input$SSP)) +
+          scale_color_viridis_d(begin = 0.4, end = 0.8) +
+          scale_linetype_manual(values = c(3, 5, 4, 1)) +
+          theme_light()
+      }
+      )
+    }
     
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
-      tabsetPanel(type = "tabs",
-                  tabPanel("Home", verbatimTextOutput("Home")),
-                  tabPanel("graphs", plotOutput(outputId = "distPlot")),
-                  tabPanel("summary", verbatimTextOutput("summary"))),
-      
-      # Output: Histogram ----
-      # plotOutput(outputId = "distPlot")
-    )
-  )
-)
-
-
-
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
+    # Create Shiny app ----
     
-    q10 <- input$Q10
+    shinyApp(ui = ui, server = server)
     
-    file <- SSP_files[input$SSP]
-    Sc <- system.file(file, package = "hector")
-    core <- newcore(Sc)
-    run(core)
-    reference <- fetchvars(core, 2000:2200, variables)
-    reference$source <- "reference"
-    setvar(core, NA, Q10_RH(), q10, getunits(Q10_RH()))
-    reset(core)
-    run(core)
-    result <- fetchvars(core, 2000:2200, variables)
-    result$source <- "user_input"
-    shutdown(core)
+    shinyApp(ui = ui, server = server)
     
-    output <- bind_rows(reference, result)
-    
-    ggplot(output, aes(year, value, color = source, linetype = units)) +
-      geom_line() +
-      facet_wrap(~variable, scales = "free") + 
-      ggtitle(paste0("Q10 = ", q10, " in scenario ", input$SSP)) +
-      scale_color_viridis_d(begin = 0.4, end = 0.8) +
-      scale_linetype_manual(values = c(3, 5, 4, 1)) +
-      theme_light()
-  }
-  )
-}
-
-# Create Shiny app ----
-
-shinyApp(ui = ui, server = server)
-
-shinyApp(ui = ui, server = server)
-
